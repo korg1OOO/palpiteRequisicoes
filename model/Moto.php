9df4b8e960f5aa1d6aaf022af64d149f52d07992
Enzo Michel Barbosa Feldman <enzomichelfeldman@gmail.com>
2025-04-12 20:42:33 -0300

    Add files via upload

---
+++ model/Moto.php
@@ -0,0 +1,43 @@
+<?php
+
+class Moto {
+    
+    private string $nome;
+    private string $link;
+
+    /**
+     * Get the value of nome
+     */
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     */
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of link
+     */
+    public function getLink(): string
+    {
+        return $this->link;
+    }
+
+    /**
+     * Set the value of link
+     */
+    public function setLink(string $link): self
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+}
